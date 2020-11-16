4a79debeb398189347d2a8f31c21225c0750a2c4
george200150 <georgeciubotariu@yahoo.com>
2020-11-16 02:01:18 +0200

    created scenes and flow. stuck at hiding squirrel (check if lesson is complete)

---
+++ Assets/Assets/Scripts/checkLearnedScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkLearnedScript : MonoBehaviour
+{
+    GameObject squirrelFW;
+    GameObject meniu;
+
+    private bool completed = false;
+
+    public void OnFocusChanged(bool hasFocus)
+    {
+        checkSkipPossible();
+    }
+
+    public void checkSkipPossible()
+    {
+        squirrelFW.SetActive(completed);
+        squirrelFW.transform.Rotate(50 * Time.deltaTime, 0, 0);
+    }
+
+    public void setCompleted()
+    {
+        this.completed = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squirrelFW = GameObject.Find("squirrel.fw");
+        meniu = GameObject.Find("meniu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        squirrelFW.transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Assets/Scripts/triggerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class triggerScript : MonoBehaviour
+{
+    GameObject squirrelFW;
+    private bool completed = false;
+
+    public void checkSkipPossible()
+    {
+        squirrelFW.SetActive(completed);
+        squirrelFW.transform.Rotate(0, 0, 50 * Time.deltaTime);
+    }
+
+    public void setCompleted()
+    {
+        this.completed = true;
+    }
+
+    void Start()
+    {
+        squirrelFW = GameObject.Find("squirrel.fw");
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerDown;
+        entry.callback.AddListener((data) => { OnPointerDownDelegate((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+        squirrelFW.transform.Rotate(0,100 * Time.deltaTime, 0);
+    }
+
+    public void OnPointerDownDelegate(PointerEventData data)
+    {
+        Debug.Log("OnPointerDownDelegate called.");
+        squirrelFW.transform.Rotate(50 * Time.deltaTime, 0, 0);
+        setCompleted();
+        checkSkipPossible();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //squirrelFW.transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+}
