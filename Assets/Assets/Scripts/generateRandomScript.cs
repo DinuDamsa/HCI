2f7ee9822125736378fa0540ca52a7d729bbe29c
george200150 <georgeciubotariu@yahoo.com>
2020-11-17 16:57:09 +0200

    adaugat joc arici

---
+++ Assets/Assets/Scripts/generateRandomScript.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class generateRandomScript : MonoBehaviour
+{
+    private GameObject animal1;
+    private GameObject animal2;
+    private GameObject animal3;
+
+    private GameObject oneFW;
+    private GameObject twoFW;
+    private GameObject threeFW;
+
+
+    private string[] animals = { "bear.fw", "boar.fw", "deer.fw", "fox.fw", "hedgehog.fw", "owl.fw", "squirrel.fw" };
+    private float[] animalSizes = { 0.2256717f, 0.5291776f, 0.7277227f, 0.2454485f, 1f, 0.4649033f, 0.3867876f };
+    private int totalAnimals = 7;
+    //private GameplayManager gameplayManager = GameObject.FindObjectOfType<GameplayManager>();
+    // https://www.youtube.com/watch?v=ck6OyNBC95c
+    public int correctNumber;
+
+    private SpriteRenderer spriteRenderer;
+    private string path = "";
+
+    private int generateRandInt(int min, int max)
+    {
+        System.Random rnd = new System.Random();
+        return rnd.Next(min, max);
+    }
+
+    private void handleShowCorrectNumberOfAnimals()
+    {
+        if (correctNumber == 1)
+        {
+            animal2.SetActive(false);
+            animal3.SetActive(false);
+        }
+        else if (correctNumber == 2)
+        {
+            animal3.SetActive(false);
+        }
+    }
+
+    private void generateRandomAnimals()
+    {
+        var renderer = animal1.GetComponent<SpriteRenderer>();
+        Debug.Log(AssetDatabase.GetAssetPath(renderer.sprite));
+        Debug.Log(renderer.sprite);
+        Debug.Log(renderer.sprite.name);
+        
+
+        int randint = generateRandInt(0, totalAnimals);
+        string animalName = animals[randint];
+        float animalSize = animalSizes[randint];
+        string pathToNewSprite = path + animalName;
+
+
+        Debug.Log(pathToNewSprite);
+        Sprite newSprite = Resources.Load<Sprite>(pathToNewSprite);
+        Debug.Log(newSprite);
+        spriteRenderer = animal1.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = newSprite;
+        spriteRenderer = animal2.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = newSprite;
+        spriteRenderer = animal3.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = newSprite;
+
+        animal1.transform.localScale = new Vector3(animalSize, animalSize, animalSize);
+        animal2.transform.localScale = new Vector3(animalSize, animalSize, animalSize);
+        animal3.transform.localScale = new Vector3(animalSize, animalSize, animalSize);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        correctNumber = generateRandInt(1,4);
+
+        animal1 = GameObject.Find("animal1");
+        animal2 = GameObject.Find("animal2");
+        animal3 = GameObject.Find("animal3");
+
+        oneFW = GameObject.Find("one.fw");
+        twoFW = GameObject.Find("two.fw");
+        threeFW = GameObject.Find("three.fw");
+
+
+        //oneFW.AddComponent(typeof(EventTrigger));
+        //EventTrigger trigger = YourGameObject.GetComponent<EventTrigger>();
+        //EventTrigger.Entry entry = new EventTrigger.Entry();
+        //entry.eventID = EventTriggerType.PointerClick;
+        //entry.callback.AddListener((eventData) => { Debug.Log("listener code");/* Your code here */ });
+        //trigger.triggers.Add(entry);
+
+
+
+        handleShowCorrectNumberOfAnimals();
+        generateRandomAnimals();
+        Debug.Log("DONE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Assets/Scripts/selectScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectScript : MonoBehaviour
+{
+    public int selectedValue;
+    public int correctNumber;
+
+    public void OnMouseUp()
+    {
+        Debug.Log(selectedValue);
+        Debug.Log(correctNumber);
+        
+        if(selectedValue == correctNumber)
+        {
+            // felicitari
+            Debug.Log("FELICITARI!");
+        }
+        else
+        {
+            // oof
+            Debug.Log("OOF!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
