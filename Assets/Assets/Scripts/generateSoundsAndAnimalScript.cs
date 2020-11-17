6f57b5d247b829c833943216547a4074cf8a26e5
george200150 <georgeciubotariu@yahoo.com>
2020-11-17 18:50:47 +0200

    added some code chunks

---
+++ Assets/Assets/Scripts/generateSoundsAndAnimalScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateSoundsAndAnimalScript : MonoBehaviour
+{
+
+    private GameObject animal;
+
+    private GameObject speaker1;
+    private GameObject speaker2;
+    private GameObject speaker3;
+
+    private string[] animals = { "bear.fw", "boar.fw", "deer.fw", "fox.fw", "hedgehog.fw", "owl.fw", "squirrel.fw" };
+    private float[] animalSizes = { 0.2256717f, 0.5291776f, 0.7277227f, 0.2454485f, 1f, 0.4649033f, 0.3867876f };
+    private int totalAnimals = 7;
+
+    public int correctNumber;
+
+    private string path = "";
+
+
+
+
+    private int generateRandInt(int min, int max)
+    {
+        System.Random rnd = new System.Random();
+        return rnd.Next(min, max);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animal = GameObject.Find("animal");
+
+        speaker1 = GameObject.Find("speaker1");
+        speaker2 = GameObject.Find("speaker2");
+        speaker3 = GameObject.Find("speaker3");
+
+        int randint = generateRandInt(0, totalAnimals);
+
+        string animalName = animals[randint];
+        string pathToNewSound = path + animalName;
+        AudioSource newAudio = Resources.Load<AudioSource>(pathToNewSound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Assets/Scripts/hoverAndClickSoundScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverAndClickSoundScript : MonoBehaviour
+{
+    private AudioSource sound;
+
+    public void OnMouseUp()
+    {
+        Debug.Log("MOUSE CLICK");
+        Debug.Log("");
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("MOUSE OVER");
+        sound.Play();
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("MOUSE EXIT");
+        sound.Stop();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
