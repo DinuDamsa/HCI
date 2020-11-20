5a616a1f4b268e5a9e51c6039daacc8ccaac36be
george200150 <georgeciubotariu@yahoo.com>
2020-11-21 00:28:15 +0200

    added replay features for animal stories

---
+++ Assets/Assets/Scripts/repeatStoryScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repeatStoryScript : MonoBehaviour
+{
+    AudioSource story;
+
+    public void OnMouseUp()
+    {
+        story.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
